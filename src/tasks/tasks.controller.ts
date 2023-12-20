1cca62cda28b0645d1b9dfe233c21ad8d7ffd32e
Lucas Koyama <lucaskoyamahhh@gmail.com>
2023-12-20 16:48:35 -0300

    controller methods made

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { TaskService } from './shared/task.service/task.service';
+import { Task } from './shared/task/task';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private taskService: TaskService) {}
+
+  @Get()
+  async getAll(): Promise<Task[]> {
+    return this.taskService.getAll();
+  }
+
+  @Get(':id')
+  async getById(@Param('id') id: number): Promise<Task> {
+    return this.taskService.getById(Number(id));
+  }
+
+  @Post()
+  async create(@Body() task: Task): Promise<Task> {
+    return this.taskService.create(task);
+  }
+
+  @Put(':id')
+  async update(@Param('id') id: number, @Body() task: Task): Promise<Task> {
+    task.id = Number(id);
+    return this.taskService.update(task);
+  }
+
+  @Delete(':id')
+  async delete(@Param('id') id: number) {
+    this.taskService.delete(Number(id));
+  }
+}
