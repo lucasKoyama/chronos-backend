3577d51bd02ee48861188276e8022c2a94264a36
Lucas Koyama <lucaskoyamahhh@gmail.com>
2023-12-24 18:38:37 -0300

    feat: exploring building API docs with swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const corsOptions: CorsOptions = {
+    origin: ['http://localhost:3000'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  };
+  app.enableCors(corsOptions);
+
+  const config = new DocumentBuilder()
+    .setTitle('Chronos API')
+    .setDescription('Documentation for Chronos API')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -11,21 +11,38 @@ import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { UpdateTaskDto } from './dto/update-task.dto';
 
+import { ApiTags, ApiResponse, ApiOperation } from '@nestjs/swagger';
+
 @Controller('tasks')
+@ApiTags('tasks')
 export class TasksController {
   constructor(private readonly tasksService: TasksService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Create a task', description: 'Create a new task.' })
+  @ApiResponse({ status: 201, description: 'Task successfully created.' })
   create(@Body() createTaskDto: CreateTaskDto) {
     return this.tasksService.create(createTaskDto);
   }
 
   @Get()
+  @ApiOperation({
+    summary: 'Get all tasks',
+    description: 'Retrieve a list of all tasks.',
+  })
   findAll() {
     return this.tasksService.findAll();
   }
 
   @Get('/:userId')
+  @ApiOperation({
+    summary: 'Get all tasks by user ID',
+    description: 'Retrieve a list of all tasks of a specified user ID',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'List of tasks successfully retrieved.',
+  })
   findAllByUserId(@Param('userId') userId: string) {
     return this.tasksService.findAllByUserId(userId);
   }
