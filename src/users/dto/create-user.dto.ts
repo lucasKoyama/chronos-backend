4eedc7b1e93145c66b1e61f5e8d9dad9a260a3ad
Lucas Koyama <lucaskoyamahhh@gmail.com>
2023-12-24 15:55:58 -0300

    feat: user class, type and DTO created

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,1 +1,24 @@
-export class CreateUserDto {}
+import { IsString, MaxLength } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString()
+  id: string;
+
+  @IsString()
+  username: string;
+
+  @IsString()
+  email: string;
+
+  @IsString()
+  passwordHash: string;
+
+  @IsString()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
+
+  @MaxLength(40, { each: true })
+  tags: Set<string>;
+}
