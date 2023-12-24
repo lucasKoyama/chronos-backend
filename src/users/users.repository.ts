de722ed2fa730df80195c2cc4ada69c1b796d50a
Lucas Koyama <lucaskoyamahhh@gmail.com>
2023-12-24 16:19:10 -0300

    feat: users defaultt architecture finished, still need to adapt to the users logic

---
+++ src/users/users.repository.ts
@@ -0,0 +1,110 @@
+import {
+  AttributeValue,
+  DeleteItemCommand,
+  DynamoDBClient,
+  GetItemCommand,
+  PutItemCommand,
+  ScanCommand,
+} from '@aws-sdk/client-dynamodb';
+import { Injectable } from '@nestjs/common';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersRepository {
+  private readonly tableName = 'users';
+  private readonly client: DynamoDBClient;
+
+  constructor() {
+    this.client = new DynamoDBClient({
+      region: 'sa-east-1',
+    });
+  }
+
+  async findAll() {
+    const result: User[] = [];
+
+    const command = new ScanCommand({
+      TableName: this.tableName,
+    });
+
+    const response = await this.client.send(command);
+
+    if (response.Items) {
+      response.Items.forEach((item) => {
+        result.push(User.newInstanceFromDynamoDBObject(item));
+      });
+    }
+
+    return result;
+  }
+
+  async findById(userId: string) {
+    const command = new GetItemCommand({
+      TableName: this.tableName,
+      Key: {
+        id: { S: userId },
+      },
+    });
+
+    const result = await this.client.send(command);
+
+    if (result.Item) {
+      return User.newInstanceFromDynamoDBObject(result.Item);
+    }
+
+    return undefined;
+  }
+
+  async upsertOne(data: User) {
+    console.log(data);
+    const itemObject: Record<string, AttributeValue> = {
+      id: {
+        S: data.id,
+      },
+      username: {
+        S: data.username,
+      },
+      email: {
+        S: data.email,
+      },
+      passwordHash: {
+        S: data.passwordHash,
+      },
+      firstName: {
+        S: data.firstName,
+      },
+      lastName: {
+        S: data.lastName,
+      },
+      tags: {
+        SS: [...data.tags],
+      },
+    };
+
+    const command = new PutItemCommand({
+      TableName: this.tableName,
+      Item: itemObject,
+    });
+
+    await this.client.send(command);
+
+    return data;
+  }
+
+  async deleteById(userId: string) {
+    const command = new DeleteItemCommand({
+      TableName: this.tableName,
+      Key: {
+        id: {
+          S: userId,
+        },
+      },
+      ReturnConsumedCapacity: 'TOTAL',
+      ReturnValues: 'ALL_OLD',
+    });
+
+    const result = await this.client.send(command);
+    if (result.Attributes) return true;
+    return false;
+  }
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,26 +1,39 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { UsersRepository } from './users.repository';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly repository: UsersRepository) {}
+
   create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+    return this.repository.upsertOne(User.newInstanceFromDTO(createUserDto));
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.repository.findAll();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  findOne(id: string) {
+    return this.repository.findById(id);
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    const existingObject = await this.repository.findById(id);
+    if (existingObject) {
+      const updateFields = Object.keys(updateUserDto);
+      updateFields.forEach(
+        (field) => (existingObject[field] = updateUserDto[field]),
+      );
+      return this.repository.upsertOne(existingObject);
+    } else {
+      return false;
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  remove(id: string) {
+    return this.repository.deleteById(id);
   }
 }
