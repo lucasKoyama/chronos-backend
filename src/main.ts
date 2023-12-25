665e92224e9de72afcdc56f55417b2e7c232aa8d
lucasKoyama <lucaskoyamahhh@gmail.com>
2023-12-25 19:07:10 -0300

    fix: CORS error

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
-    origin: ['https://chronos-backend.vercel.app/'],
+    origin: ['http://localhost:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   };
