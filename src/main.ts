edd24e0041745ca857eb127463059373149f1489
Lucas Koyama <lucaskoyamahhh@gmail.com>
2023-12-26 20:49:37 -0300

    fix: problems with CORS during frontend development

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
-    origin: ['http://localhost:3000'],
+    origin: ['http://192.168.200.105:3000'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   };
